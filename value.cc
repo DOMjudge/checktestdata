bc38b657da424fc4b60f23f37049f2584fb30ce8
Tobias Polzer <tobiasp@google.com>
2018-06-30 00:37:03 +0200

    Travis

--- value.cc
+++ value.cc
@@ -9,7 +9,7 @@ Value Value::negate() const {
         } else if constexpr (is_integral<T>) {
           if (v >= std::numeric_limits<int64_t>::min() + 1 &&
               -v >= std::numeric_limits<int64_t>::min()) {
-            return Value{int64_t{-v}};
+            return Value{int64_t{T{-v}}};
           } else {
             return Value{-integer{v}};
           }
