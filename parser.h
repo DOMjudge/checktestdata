409649dc32f36405fbc26ccdd4a6d7f38ad965df
Jaap Eldering <jaap@jaapeldering.nl>
2019-10-20 23:53:57 +0200

    Fix 3b6ed3cfb2f2c970fb: keep old Bisonc++/Flexc++ code.
    Also drop a C++17 [[maybe_unused]] attribute that generates a
    compiler error on some old compilers (GCC 4.8.4 on Travis CI).

--- parser.h
+++ parser.h
@@ -32,18 +32,34 @@ class Parser: public ParserBase
         parse_t parseResult;
 
     private:
+#if ( BISONCPP_VERSION >= 60000LL )
         void error();                   // called on (syntax) errors
+#else
+        void error(char const *msg);
+#endif
         int lex();                      // returns the next token from the
                                         // lexical scanner.
         void print();                   // use, e.g., d_token, d_loc
+#if ( BISONCPP_VERSION >= 60000LL )
         void exceptionHandler(std::exception const &exc);
+#else
+        void exceptionHandler__(std::exception const &exc);
+#endif
 
     // support functions for parse():
+#if ( BISONCPP_VERSION >= 60000LL )
         void executeAction_(int ruleNr);
         void errorRecovery_();
         void nextCycle_();
         void nextToken_();
         void print_();
+#else
+        void executeAction(int ruleNr);
+        void errorRecovery();
+        int lookup(bool recovery);
+        void nextToken();
+        void print__();
+#endif
 };
 
 
